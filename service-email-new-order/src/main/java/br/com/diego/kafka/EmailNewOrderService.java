4859a2b0d74cf9d143b2a9fbd2a6d44860662750
Diego <dcandido@ciandt.com>
2021-10-26 11:00:04 -0300

    Ajustando Offset para Latest

--- service-email-new-order/src/main/java/br/com/diego/kafka/EmailNewOrderService.java
+++ service-email-new-order/src/main/java/br/com/diego/kafka/EmailNewOrderService.java
@@ -1,31 +1,24 @@
 package br.com.diego.kafka;
 
-import br.com.diego.kafka.consumer.KafkaService;
+import br.com.diego.kafka.consumer.ConsumerService;
+import br.com.diego.kafka.consumer.ServiceRunner;
 import br.com.diego.kafka.dispatcher.KafkaDispatcher;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.sql.SQLException;
-import java.util.Map;
 import java.util.concurrent.ExecutionException;
 
-public class EmailNewOrderService {
+public class EmailNewOrderService implements ConsumerService<Order> {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(EmailNewOrderService.class);
     private final KafkaDispatcher<String> emailDispatcher = new KafkaDispatcher<>();
 
-    public static void main(String[] args) throws ExecutionException, InterruptedException, SQLException {
-        var emailService = new EmailNewOrderService();
-        try (var service = new KafkaService(EmailNewOrderService.class.getSimpleName(),
-                "ECOMMERCE_NEW_ORDER",
-                emailService::parse,
-                Map.of())) {
-            service.run();
-        }
+    public static void main(String[] args) {
+        new ServiceRunner(EmailNewOrderService::new).start(1);
     }
 
-    private void parse(ConsumerRecord<String, Message<Order>> record) throws ExecutionException, InterruptedException {
+    public void parse(ConsumerRecord<String, Message<Order>> record) throws ExecutionException, InterruptedException {
         LOGGER.info("-------------------------");
         LOGGER.info("PROCESSING NEW ORDER, PREPARING EMAIL");
         var message = record.value();
@@ -37,5 +30,15 @@ public class EmailNewOrderService {
         emailDispatcher.send("ECOMMERCE_SEND_EMAIL", order.getEmail(), id, emailCode);
 
         LOGGER.info("EMAIL ENVIADO COM SUCESSO");
+    }
+
+    @Override
+    public String getTopic() {
+        return "ECOMMERCE_NEW_ORDER";
+    }
+
+    @Override
+    public String getConsumerGroup() {
+        return EmailNewOrderService.class.getSimpleName();
     }
 }
